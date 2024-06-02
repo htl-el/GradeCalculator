ae9841b4ce309daf7744295d20eb2581287060c6
Fabian Schieder <fabian.schieder@el.htlhl.at>
2024-06-02 20:00:00 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "Application.hpp"
+#include "Database.hpp"
 
 #include <QApplication>
 
@@ -7,5 +8,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Application w;
     w.show();
+
+    Database data("list.txt");
+    data.addSubject("Mathe", 5);
+
     return a.exec();
 }
